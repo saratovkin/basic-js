fb3ee10aff666a4e27b8b8a89bb80bd882943a5c
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-09-14 15:16:59 +0300

    feat: add solution to Carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,10 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if (sampleActivity > MODERN_ACTIVITY) {
+    return false;
+  }
+  let result = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+  return (isNaN(result) || !isFinite(result)) ? false : result;
 }
