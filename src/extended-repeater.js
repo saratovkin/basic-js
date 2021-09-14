e71b7ca19bc77dc7c483e61e0f9b48837e4595d6
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-09-14 14:15:27 +0300

    feat: add solution to Extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,36 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str,options) {
+  if (options.repeatTimes === undefined) {
+    options.repeatTimes = 0;
+  }
+  if (options.separator === undefined) {
+    options.separator = '+';
+  }
+  if (options.addition === undefined) {
+    options.addition = '';
+  }
+  if (options.additionRepeatTimes === undefined) {
+    options.additionRepeatTimes = 0;
+  }
+  if (options.additionSeparator === undefined) {
+    options.additionSeparator = '|';
+  }
+  if (options.addition != '') {
+    for (let i = 0; i < options.additionRepeatTimes; i++) {
+      str += options.addition + options.additionSeparator;
+    }
+    str = str.substring(0, str.length - options.additionSeparator.length);
+  }
+  if (options.repeatTimes != 0) {
+    let result = '';
+    for (let i = 0; i < options.repeatTimes; i++) {
+      result += str + options.separator;
+    }
+    result = result.substring(0, result.length - options.separator.length);
+    str = result;
+  }
+  return str;
 }
+
