046cb4c3cd832436be1bfef1e755ad3abaf226be
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-09-14 22:35:38 +0300

    feat: add solution to (ST) Encode line task

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let result = [];
+  str.split('').forEach((item) => {
+    let reg = new RegExp(item, 'g');
+    result.push(str.match(reg).length + item);
+  });
+  function unique(value, index, self) {
+    return self.indexOf(value) === index;
+  }
+  return result.filter(unique).join('');
 }
+
