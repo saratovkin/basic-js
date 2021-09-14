64ecc02f362f5eac5f0d288ad657f3b4a8defea2
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-09-14 16:22:31 +0300

    fix: fix solution to Common characters count task

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -11,13 +11,14 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(s1, s2) {
+export default  function getCommonCharacterCount(s1, s2) {
   let array1 = s1.split('');
   let array2 = s2.split('');
   let counter = 0;
   for (let i = 0; i < array1.length; i++) {
     for (let j = 0; j < array2.length; j++) {
       if (array1[i] == array2[j]) {
+        counter++;
         array1[i] = null;
         array2[j] = '';
       }
