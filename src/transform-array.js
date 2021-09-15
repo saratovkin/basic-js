d6c5d6738200f47c6fdc5254422a354664b435df
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-09-15 17:02:47 +0300

    fix: fix solution to Transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -20,33 +20,33 @@ export default function transform(arr) {
   }
   result = [...arr];
   let index = 0;
+  if (result.indexOf('--double-next') != -1) {
+    index = result.indexOf('--double-next');
+    result.splice(index, 1);
+    if (index != result.length) {
+      result.splice(index, 0, result[index]);
+    }
+  }
+  if (result.indexOf('--discard-next') != -1) {
+    index = result.indexOf('--discard-next');
+    result.splice(index, 1);
+    if (!(index == result.length || index == result.indexOf('--discard-prev') - 1)) {
+      result.splice(index, 1);
+    }
+  }
   if (result.indexOf('--discard-prev') != -1) {
     index = result.indexOf('--discard-prev');
     result.splice(index, 1);
     if (index != 0) {
       result.splice(index - 1, 1);
     }
-  }
-  if (result.indexOf('--discard-next') != -1) {
-    index = result.indexOf('--discard-next');
-    result.splice(index, 1);
-    if (index != result.length) {
-      result.splice(index, 1);
-    }
+
   }
   if (result.indexOf('--double-prev') != -1) {
     index = result.indexOf('--double-prev');
     result.splice(index, 1);
-    console.log(result);
-    if (index != 0) {
+    if (!(index == 0 || index == result.indexOf('--discard-next') + 1)) {
       result.splice(index - 1, 0, result[index - 1]);
-    }
-  }
-  if (result.indexOf('--double-next') != -1) {
-    index = result.indexOf('--double-next');
-    result.splice(index, 1);
-    if (index != result.length) {
-      result.splice(index, 0, result[index]);
     }
   }
   return result;
