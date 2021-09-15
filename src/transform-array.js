b49ca6493c4ebd9211e40a896c2b06672b85ebc8
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-09-15 16:12:57 +0300

    feat: add solution to Transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -4,7 +4,7 @@ import { NotImplementedError } from '../extensions/index.js';
  * Create transformed array based on the control sequences that original
  * array contains
  * 
- * @param {Array} arr initial array
+ * @param {Array} result initial array
  * @returns {Array} transformed array
  * 
  * @example
@@ -13,7 +13,40 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('arr parameter must be an instance of the Array!');
+  }
+  result = [...arr];
+  let index;
+  if (result.indexOf('--discard-prev') != -1) {
+    index = result.indexOf('--discard-prev');
+    result.splice(index, 1);
+    if (index != 0) {
+      result.splice(index - 1, 1);
+    }
+  }
+  if (result.indexOf('--discard-next') != -1) {
+    index = result.indexOf('--discard-next');
+    result.splice(index, 1);
+    if (index != result.length) {
+      result.splice(index, 1);
+    }
+  }
+  if (result.indexOf('--double-prev') != -1) {
+    index = result.indexOf('--double-prev');
+    result.splice(index, 1);
+    console.log(result);
+    if (index != 0) {
+      result.splice(index - 1, 0, result[index - 1]);
+    }
+  }
+  if (result.indexOf('--double-next') != -1) {
+    index = result.indexOf('--double-next');
+    result.splice(index, 1);
+    if (index != result.length) {
+      result.splice(index, 0, result[index]);
+    }
+  }
+  return result;
 }
