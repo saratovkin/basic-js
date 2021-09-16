f39487145c042c946701997cd7bed650f595772c
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-09-16 18:20:17 +0300

    feat: add Vigenere cipher task solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,13 +19,84 @@ import { NotImplementedError } from '../extensions/index.js';
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
+String.prototype.replaceAt = function (index, replacement) {
+  return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+}
+
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(flag) {
+    this.flag = flag;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(str, key) {
+    if (str === undefined || key === undefined) {
+      throw new Error("Incorrect arguments!");
+    }
+    let strCode;
+    let keyCode;
+    let finalCode;
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    while (str.length > key.length) {
+      key += key;
+    }
+    for (let i = 0, j = 0; i < str.length; i++, j++) {
+      strCode = str[i].charCodeAt(0) - 65;
+      keyCode = key[j].charCodeAt(0) - 65;
+      if (strCode >= 0) {
+        if (strCode + keyCode > 25) {
+          finalCode = strCode + keyCode - 26 + 65;
+        } else {
+          finalCode = strCode + keyCode + 65;
+        }
+        str = str.replaceAt(i, String.fromCharCode(finalCode));
+      } else {
+        j--;
+      }
+    }
+    if (this.flag === false) {
+      str = str.split('').reverse().join('');
+      key = key.split('').reverse().join('');
+    }
+    return str;
+  }
+  decrypt(str, key) {
+    if (str === undefined || key === undefined) {
+      throw new Error("Incorrect arguments!");
+    }
+    let strCode;
+    let keyCode;
+    let finalCode;
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    while (str.length > key.length) {
+      key += key;
+    }
+    for (let i = 0, j = 0; i < str.length; i++, j++) {
+      strCode = str[i].charCodeAt(0) - 65;
+      keyCode = key[j].charCodeAt(0) - 65;
+      if (strCode >= 0) {
+        if (strCode < keyCode) {
+          finalCode = 26 - keyCode + strCode;
+        } else {
+          finalCode = strCode - keyCode;
+        }
+        str = str.replaceAt(i, String.fromCharCode(finalCode + 65));
+      } else {
+        j--;
+      }
+    }
+    if (this.flag === false) {
+      str = str.split('').reverse().join('');
+      key = key.split('').reverse().join('');
+    }
+    return str;
   }
 }
+
+
+
+
+
+
+
