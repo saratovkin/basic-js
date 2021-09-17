1140012701f27f06463093af2c44664e89dc21fe
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-09-17 16:14:42 +0300

    fix: test Count cats task solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -15,14 +15,14 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default function countCats(matrix) {
-  let counter = 0;
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      if (matrix[i][j]== '^^') {
-        counter++;
-      }
-    }
-  }
-  return counter;
+  // let counter = 0;
+  // for (let i = 0; i < matrix.length; i++) {
+  //   for (let j = 0; j < matrix[i].length; j++) {
+  //     if (matrix[i][j]== '^^') {
+  //       counter++;
+  //     }
+  //   }
+  // }
+  // return counter;
 }
 
