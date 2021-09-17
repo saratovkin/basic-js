baba5004728132b581a91f3f3fb4d15cd971e46e
Артур Саратовкин <artursaratovkin@MacBook-Pro-Artur.local>
2021-09-17 16:30:45 +0300

    fix: test Hanoi tower task solution

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,14 +14,7 @@ import { NotImplementedError } from '../extensions/index.js';
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-export default function calculateHanoi(disksNumber, turnsSpeed) {
-  let turns = 1;
-  let result = {};
-  for (let i = 1; i <= disksNumber; i++) {
-    turns = Math.pow(2, i);
-  }
-  result.turns = turns - 1;
-  turnsSpeed = turnsSpeed / 3600;
-  result.seconds = Math.floor(result.turns / turnsSpeed);
-  return result;
+export default function calculateHanoi(/* disksNumber, turnsSpeed */) {
+  throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
 }
